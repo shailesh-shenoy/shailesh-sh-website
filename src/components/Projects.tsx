ee970078a01be23bda809af74166df27a5c32436
Shailesh Shenoy <shenoy.sh@northeastern.edu>
2024-01-13 14:41:29 -0500

    chain images

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,18 +1,16 @@
-import { Text, Heading, SimpleGrid, Stack } from "@chakra-ui/react";
+import { Text, Heading, SimpleGrid, Stack, Flex, Box } from "@chakra-ui/react";
 import { projects } from "./types";
 import Project from "./Project";
 export default function Projects() {
-    return (
-        <Stack as="section" w={"100%"} maxW="8xl" bg="white" p={4}>
-            <Heading as="h2" size="lg" mb={4}>
-                Projects
-            </Heading>
-            <Text>Check out some of my recent work!</Text>
-            <SimpleGrid columns={{ "base": 1, md: 2, lg: 3 }} spacing={8}>
-                {projects.map((project) => (
-                    Project(project)
-                ))}
-            </SimpleGrid>
-        </Stack>
-    );
-};
+  return (
+    <Box as="section" w={"100%"} maxW="8xl" bg="white" p={4}>
+      <Heading as="h2" size="lg" mb={4}>
+        Projects
+      </Heading>
+      <Text>Check out some of my recent work!</Text>
+      <Flex direction="column" align="stretch" gap={8}>
+        {projects.map((project) => Project(project))}
+      </Flex>
+    </Box>
+  );
+}

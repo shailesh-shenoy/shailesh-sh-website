5f42f62b610551e9b65801180659e7752b9d4e5e
Shailesh Shenoy <shenoy.sh@northeastern.edu>
2024-01-17 17:52:02 -0500

    Updates contact text

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -17,7 +17,11 @@ export default function Contact() {
   return (
     <Stack as="section" w={"100%"} maxW="4xl" bg="white" p={4} mb={16}>
       <Heading>Contact Me</Heading>
-      <Text>Want to talk tech or build something with me?</Text>
+      <Text>
+        I am currently open to opportunities in Software as well as Web3 space.
+        Feel free to reach out if you want to build something with me or just
+        say Hi!
+      </Text>
       <Button
         as={NextLink}
         href={mailToText}
@@ -27,7 +31,7 @@ export default function Contact() {
         w={40}
         textAlign="center"
       >
-        Reach out to me
+        Get in touch
       </Button>
     </Stack>
   );

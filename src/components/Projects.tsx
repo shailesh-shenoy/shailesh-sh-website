6a320b3d5ecccad7eb58fab05be916bedbd6fee2
shailesh-shenoy <shenoy.shailesh97@gmail.com>
2024-01-22 20:51:12 -0500

    Updated projects section color

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -4,15 +4,17 @@ import Project from "./Project";
 export default function Projects() {
     return (
         <Box as="section" id="projects" w={"100%"} maxW="4xl" bgImage="linear-gradient(
-            45deg,
-            hsl(16deg 100% 63%) 0%,
-            hsl(14deg 100% 69%) 1%,
-            hsl(12deg 100% 75%) 3%,
-            hsl(10deg 100% 81%) 5%,
-            hsl(8deg 100% 86%) 8%,
-            hsl(5deg 100% 91%) 14%,
-            hsl(3deg 100% 95%) 30%,
-            hsl(0deg 14% 99%) 100%
+            225deg,
+            hsl(43deg 76% 81%) 0%,
+            hsl(42deg 78% 83%) 11%,
+            hsl(41deg 80% 84%) 22%,
+            hsl(40deg 82% 86%) 33%,
+            hsl(38deg 84% 88%) 44%,
+            hsl(37deg 86% 90%) 56%,
+            hsl(36deg 87% 91%) 67%,
+            hsl(35deg 88% 93%) 78%,
+            hsl(34deg 86% 94%) 89%,
+            hsl(33deg 82% 96%) 100%
           )" p={8} border={2} borderStyle="solid" borderColor="#FF8066">
             <Heading as="h2" fontSize="3xl" mb={8} textAlign="center" fontFamily="secondary" fontWeight="500" lineHeight={1.4}>
                 Projects
